2fcf220a551caf05fbad81e2a4fe26aa84510905
Alejadnro Macari <macari1996@gmail.com>
2020-04-21 02:09:28 -0400

    Updates

---
+++ precache-manifest.42d48ad782cab5e8c09fb59a3bddbda1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7672476db1f5bc5571d9db92a97f91",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "d6e759e03dac1b23ac69",
+    "url": "/react-monsters/static/css/main.764aaf3b.chunk.css"
+  },
+  {
+    "revision": "20b1912bf242704f9f11",
+    "url": "/react-monsters/static/js/2.0a6b9ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.0a6b9ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e759e03dac1b23ac69",
+    "url": "/react-monsters/static/js/main.e821f720.chunk.js"
+  },
+  {
+    "revision": "824d8fe815a28c2f2192",
+    "url": "/react-monsters/static/js/runtime-main.2adf1e65.js"
+  }
+]);
